68382324dee093246dff16c0238cf514841301ea
pjs <dhs@DESKTOP-DHSHOME>
2023-09-12 23:44:16 +0900

    sql 파일 업로드. create 등등.

---
+++ adventour/WebContent/WEB-INF/resoo/Create etc something.sql
@@ -0,0 +1,23 @@
+ 
+use adventour; 
+
+
+CREATE TABLE member (
+  m_id varchar(20) NOT NULL,
+  m_pw varchar(20) NOT NULL,
+  m_lastname varchar(20) NOT NULL,
+  m_firstname varchar(20) NOT NULL,
+  m_nickname varchar(10) ,
+  m_postcode varchar(50) NOT NULL,
+  m_addr1 varchar(50) NOT NULL,
+  m_addr2 varchar(50) NOT NULL,
+  m_pum1  varchar(11) NOT NULL,
+  m_pnum2 varchar(11),
+  m_email varchar(100) NOT NULL,
+  m_birth_y varchar(4),
+  m_birth_m varchar(2),
+  m_birth_d varchar(2),
+  m_gender varchar(1),
+  m_agree varchar(1),
+ PRIMARY KEY(m_id);
+);
