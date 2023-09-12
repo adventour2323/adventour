eb93b38a4e1092f9386eeb38620c20a0dde275ea
adventour2323 <144432274+adventour2323@users.noreply.github.com>
2023-09-12 23:30:35 +0900

    Update member.js
    수동

--- adventour/WebContent/mem/member.js
+++ adventour/WebContent/mem/member.js
@@ -1,8 +1,14 @@
 window.onload = function () {
 	var mship = document.querySelectorAll("input[required],select[required]");
-	var idBtn = document.querySelector("form input:nth-child(4)");
+//	var idBtn = document.getElementsByName("m_id_btn")[0];
 	var mBtn = document.getElementsByName("msBtn")[0];
 //	var init = document.getElementById("init");
+
+	
+//	id입력 input에 onKeyup으로 중복체크 이벤트 걸기
+	var idTB = document.getElementsByName("m_id")[0];
+	idTB.onkeyup = idCheckF;
+//	idTB.addEventLis,,,
 	
 	
 	mBtn.onclick = function () {
@@ -45,37 +51,33 @@ window.onload = function () {
 		
 		if (no_blank) {
 			alert('회원가입이 완료되었습니다.' + all_print);
-			
-		} 
-		
-		else {
-			alert('회원가입이 실패되었습니다.' + ms.title + '를 확인해주세요.');
+		}else {
+			alert('회원가입이 실패되었습니다.' + ms.title + '을(를) 확인해주세요.');
 		}
-		
-//		var myForm = $('form[name=myForm]');
-		$.ajax({
-		    url: '/adventour/mem/member_pjs.jsp',
-//		    type: 'GET',//POST
-		    type: 'POST',
-//		    dataType: 'json',
-		    dataType: 'text',
-		    data: $('form[name=myForm]').serialize(),
-		    success: function(data) {
-//		        console.log(data);
-		        if(data.indexOf('sucess')){
-		        	alert();
-		        }
-		        
-		    },
-		    error: function(error) {
-		        console.error('Request error:', error);
-		    }
-		});
-		
 	}
-	// rPw.addEventListener('keydown',function () {
+		/*		var myForm = $('form[name=myForm]');
+				$.ajax({
+				    url: '/adventour/mem/member_pjs.jsp',
+				    type: 'GET',//POST
+				    type: 'POST',
+				    dataType: 'json',
+				    dataType: 'text',
+				    data: $('form[name=myForm]').serialize(),
+				    success: function(data) {
+				        console.log(data);
+				        if(data.indexOf('sucess')){
+				        	alert();
+				        }
+				        
+				    },
+				    error: function(error) {
+				        console.error('Request error:', error);
+				    }
+				});*/
+		
 	
-		// });
+	
+
 	
 //	생년월일 콤보박스 값 세팅
 	for (var i = 2021; i > 1920; i--) {
@@ -89,23 +91,6 @@ window.onload = function () {
 	}
 		
 
-
-//		init.onclick = function () {
-//			for (var ms of mship) {
-//				ms.value = generateRandomString(5);
-//			}
-//		}
-//		
-//	function generateRandomString(length) {
-//		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//		let result = '';
-//		for (let i = 0; i < length; i++) {
-//			const randomIndex = Math.floor(Math.random() * characters.length);
-//			result += characters.charAt(randomIndex);
-//		}
-//		return result;
-//	}
-	
 	
 //	모두 동의합니다 체크박스 클릭 이벤트
 	$('input#all_cked').click(function(){
@@ -122,20 +107,17 @@ window.onload = function () {
 	});
 
 	
-//	no_korean 클래스인 input박스들에 모두 키다운 이벤트로 숫자만 입력되도록
+//	no_korean 클래스인 input 이벤트로 한글만 입력 안 되도록
 	const no_korean = document.querySelectorAll('.no_korean');
 	
 	for (var i = 0; i < no_korean.length; i++) {
 		no_korean[i].addEventListener('input', function(e) {
-			// 입력된 값에서 숫자만 가져오기
-//			e.target.value = e.target.value.replace(/[가-힣ㄱ-ㅎㅏ-ㅣ]/g, '');
-//			e.target.value = e.target.value.replace(/[가-힣ㄱ-ㅎㅏ-ㅣ]/g, '');
+			// 입력된 값에서 한글만 제거하기
 			e.target.value = e.target.value.replace(/[가-힣ㄱ-ㅎㅏ-ㅣ]/g, '');
-
 		});
 	}
 	
-//	pnum_check 클래스인 input박스들에 모두 키다운 이벤트로 숫자만 입력되도록
+//	pnum_check 클래스인 input이벤트로 숫자만 입력되도록
 	const numberInputs = document.querySelectorAll('.pnum_check');
 
 	for (var i = 0; i < numberInputs.length; i++) {
@@ -154,8 +136,10 @@ window.onload = function () {
 		var rPw = document.querySelectorAll("input[type=password]")[1];
 		var result = document.getElementById("result");
 		if (pw.value == rPw.value) {
+			result.style.color ="green";
 			result.innerHTML = "입력한 비밀번호가 동일합니다.";
 		} else {
+			result.style.color ="tomato";
 			result.innerHTML = "비밀번호를 다시 입력해주세요.";
 		}
 	}//내가 하고 싶은 것은 일단 비밀번호밸류랑 비밀번호확인 밸류를 가져와서. 두개를 비교하고 싶음. 
@@ -166,6 +150,64 @@ window.onload = function () {
 	
 	
 }
+
+//아이디 중복 검사 ajax
+let idCheck = false;
+function idCheckF() {
+    let m_id = $('#m_id').val();
+    $.ajax({
+        url: 'mem/member_id_check_pjs.jsp',
+        type: 'post',
+        data: {"m_id": m_id},
+        aSync: false, // ajax 통신이 끝나야 다음줄 if(idCheck){ 가 실행되도록
+        success: function (cnt) {
+            if (cnt != 1) {
+                $('.id_check').html('사용 가능한 아이디입니다.').css('color','green');
+                idCheck = true;
+            } else {
+                $('.id_check').html('이미 사용하고 있는 아이디입니다.').css('color','tomato');
+                idCheck = false;
+            }
+        },
+	    error: function(error) {
+	        console.error('Request error:', error);
+	    }
+    });
+    if(idCheck){
+    	return true;
+    }else{
+    	return false;
+    }
+}
+
+function validation(){
+	
+//	idCheckF();
+	var valiRequireds = true;
+//	var myForm = document.myForm;
+	$(document.myForm).find('input').each(function(){
+	    if(this.required){
+	        if(this.value == ''){
+	        	valiRequireds = false;
+	        	alert(this.title + '값은 필수입니다.');
+	        	return false;
+	        }
+	    }
+	});
+	
+	if(valiRequireds){
+		document.myForm.submit();
+		return true;
+	}else {
+//		빈칸이 있어요
+		
+		return false;
+	}
+	
+	
+	
+}
+
 	//본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
 	function sample4_execDaumPostcode() {
 		new daum.Postcode({
@@ -221,3 +263,4 @@ window.onload = function () {
 			}
 		}).open();
 	}
+	
