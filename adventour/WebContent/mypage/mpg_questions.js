16591860a9295a99b67a62d797992e32bf1ee746
pjs <Administrator@User -2023QVKLK>
2023-09-14 11:48:24 +0900

    중간중간 커밋하는 중.

---
+++ adventour/WebContent/mypage/mpg_questions.js
@@ -0,0 +1,7 @@
+/*mpg_questions.js*/
+
+$(document).ready(function() {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+    $("#mpg_nvbox").load("mpg_nvbox.html");
+});
